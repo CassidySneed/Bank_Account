20baddabc89ad0884193a85774a0974c8f066dea
CassidySneed <teamsneedman@gmail.com>
2022-10-31 16:03:02 -0700

    updated bank account function

--- BankAccount.py
+++ BankAccount.py
@@ -38,23 +38,26 @@ class BankAccount:
     # how to sensitized the bank account number 
     # Print Statement 
     def print_statment(self):
-        print(f"{self.full_name} Account No.: {self.account_number} Balance: ${round(self.balance, 2)}")
+        # hide first four digits 
+        hidden = str(self.account_number)[4:8]
+        hidden = "****" + hidden
+        print(f"{self.full_name} Account No.: {hidden} Balance: ${round(self.balance, 2)}")
 
 
 # why is it printing insuffient funds and why is it not rounding 
-mitchell_hudson = BankAccount("Mitchell Hudson", "1", 0)
-mitchell_hudson.deposit(400000)
-mitchell_hudson.print_statment()
-mitchell_hudson.add_interest()
-mitchell_hudson.withdraw(150)
-mitchell_hudson.print_statment()
+# mitchell_hudson = BankAccount("Mitchell Hudson", "1", 0)
+# mitchell_hudson.deposit(400000)
+# mitchell_hudson.print_statment()
+# mitchell_hudson.add_interest()
+# mitchell_hudson.withdraw(150)
+# mitchell_hudson.print_statment()
 
-jennifer_carrneo = BankAccount("Jennifer Carreno", "2", 0)
-jennifer_carrneo.deposit(500)
-jennifer_carrneo.add_interest()
-jennifer_carrneo.print_statment()
-jennifer_carrneo.withdraw(550)
-jennifer_carrneo.print_statment()
+# jennifer_carrneo = BankAccount("Jennifer Carreno", "2", 0)
+# jennifer_carrneo.deposit(500)
+# jennifer_carrneo.add_interest()
+# jennifer_carrneo.print_statment()
+# jennifer_carrneo.withdraw(550)
+# jennifer_carrneo.print_statment()
 
 stella_collins = BankAccount('Stella Collins', "3", 0)
 stella_collins.deposit(300)
