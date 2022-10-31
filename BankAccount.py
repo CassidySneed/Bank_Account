c66b9b33c28d40256079142ffc6d012dfd904ea5
CassidySneed <teamsneedman@gmail.com>
2022-10-30 21:34:36 -0700

    updated functions

--- BankAccount.py
+++ BankAccount.py
@@ -6,47 +6,70 @@ import random
 class BankAccount: 
     def __init__(self, full_name, account_number, balance=0): 
         self.full_name = full_name
-        self.account_number = account_number
+        self.account_number = random.randrange(10000000, 99999999)
         self.balance = balance 
 
 
-# Deposit 
-def deposit(self, amount):
-    self.balance += amount
-    print(f'Amount deposited:{amount}. New balance {self.balance}')
+    # Deposit 
+    def deposit(self, amount):
+        self.balance += amount
+        print(f'Amount deposited: ${amount}. New balance ${self.balance}')
 
-# Withdraw 
-def withdraw(self, amount):
-    self.balance -= amount
-    if self.balance < amount: 
-        self.balance -= 10 
-        print ('Insuffient Funds')
-    else: 
-        print(f'Amount withdrawn:{amount}. New balance: {self.balance}')
+    # Withdraw 
+    def withdraw(self, amount):
+        self.balance -= amount
+        if self.balance < amount: 
+            self.balance -= 10 
+            print ('Insuffient Funds')
+        else: 
+            print(f'Amount withdrawn: ${amount}. New balance: ${self.balance}')
 
-# Get Balance 
-def get_balance(self): 
-    print(f'Your balance is: {self.balance}')
-    return balance
+    # Get Balance 
+    def get_balance(self): 
+        print(f'Hey there! Your balance is: ${self.balance}')
+        return self.balance
 
-# Add Interest 
-def add_interest(self): 
-    interest = balance * 0.00083
+    # Add Interest 
+    def add_interest(self): 
+        interest = round(self.balance * 0.00083, 2)
+        self.balance += interest
+        print(f'With interesed added. Balance: ${self.balance}')
 
-# Print Statement 
-def print_statment(self):
-    print(f"{self.name} Account Number: {self.account} Balance: {self.balance}")
-
-
-
-
-
-# Instatiation 
-# number = random.randint(0,9)
+    # how to sensitized the bank account number 
+    # Print Statement 
+    def print_statment(self):
+        print(f"{self.full_name} Account No.: {self.account_number} Balance: ${self.balance}")
 
 
 
-mitchell_hudson = BankAccount("Mitchell Hudson", "03141592", 0)
+
+
+mitchell_hudson = BankAccount("Mitchell Hudson", "1", 0)
+mitchell_hudson.deposit(400000)
 mitchell_hudson.print_statment()
+mitchell_hudson.add_interest()
+mitchell_hudson.withdraw(150)
+mitchell_hudson.print_statment()
+
+jennifer_carrneo = BankAccount("Jennifer Carreno", "2", 0)
+jennifer_carrneo.deposit(500)
+jennifer_carrneo.add_interest()
+jennifer_carrneo.print_statment()
+
+stella_collins = BankAccount('Stella Collins', "3", 0)
+
+
+
+
+
+
+# mitchell_hudson.withdraw(50)
+# mitchell_hudson.get_balance()
+# mitchell_hudson.add_interest()
+# mitchell_hudson.print_statment()
+
+
+
+
 
 
